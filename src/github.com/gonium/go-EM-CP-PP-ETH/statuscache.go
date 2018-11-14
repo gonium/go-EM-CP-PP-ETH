6cb44b7585a2a80e97c4e17f33a7473b96b7dd36
Mathias Dalheimer <md@gonium.net>
2018-11-14 15:28:26 +0100

    fiddling with modbus codes

--- src/github.com/gonium/go-EM-CP-PP-ETH/statuscache.go
+++ src/github.com/gonium/go-EM-CP-PP-ETH/statuscache.go
@@ -48,23 +48,23 @@ type Status struct {
 	DIPConfiguration      uint16
 	FirmwareVersion       uint32
 	Errorcode             Errorcode
-	L1Voltage             uint32
-	L2Voltage             uint32
-	L3Voltage             uint32
-	L1Current             uint32
-	L2Current             uint32
-	L3Current             uint32
-	ActivePower           uint32
-	ReactivePower         uint32
-	ApparentPower         uint32
-	PowerFactor           uint32
-	Energy                uint32
-	MaxPower              uint32
-	CurrentChargePower    uint32
-	Frequency             uint32
-	L1MaxPower            uint32
-	L2MaxPower            uint32
-	L3MaxPower            uint32
+	L1Voltage             float32
+	L2Voltage             float32
+	L3Voltage             float32
+	L1Current             float32
+	L2Current             float32
+	L3Current             float32
+	ActivePower           float32
+	ReactivePower         float32
+	ApparentPower         float32
+	PowerFactor           float32
+	Energy                float32
+	MaxPower              float32
+	CurrentChargePower    float32
+	Frequency             float32
+	L1MaxPower            float32
+	L2MaxPower            float32
+	L3MaxPower            float32
 	OverCurrentProtection uint16
 	DigitalInputStates    DigiInputs
 	DigitalOutputStates   DigiOutputs
@@ -259,26 +259,56 @@ func (sc *StatusCache) parseInputRegisterStatus(input []byte) (err error) {
 	sc.Status.Errorcode.CPNoDiode = checkUint16MaskAndSet(state,
 		ERROR_CP_NO_DIODE)
 
-	sc.Status.L1Voltage = binary.BigEndian.Uint32(input[16:20])
-	sc.Status.L2Voltage = binary.BigEndian.Uint32(input[20:24])
-	sc.Status.L3Voltage = binary.BigEndian.Uint32(input[24:28])
-	sc.Status.L1Current = binary.BigEndian.Uint32(input[28:32])
-	sc.Status.L2Current = binary.BigEndian.Uint32(input[32:36])
-	sc.Status.L3Current = binary.BigEndian.Uint32(input[36:40])
-	sc.Status.ActivePower = binary.BigEndian.Uint32(input[40:44])
-	sc.Status.ReactivePower = binary.BigEndian.Uint32(input[44:48])
-	sc.Status.ApparentPower = binary.BigEndian.Uint32(input[48:52])
-	sc.Status.PowerFactor = binary.BigEndian.Uint32(input[52:56])
-	sc.Status.Energy = binary.BigEndian.Uint32(input[56:60])
-	sc.Status.MaxPower = binary.BigEndian.Uint32(input[60:64])
-	sc.Status.CurrentChargePower = binary.BigEndian.Uint32(input[64:68])
-	sc.Status.Frequency = binary.BigEndian.Uint32(input[68:72])
-	sc.Status.L1MaxPower = binary.BigEndian.Uint32(input[72:76])
-	sc.Status.L2MaxPower = binary.BigEndian.Uint32(input[76:80])
-	sc.Status.L3MaxPower = binary.BigEndian.Uint32(input[80:84])
+	sc.Status.L1Voltage =
+		float32(binary.BigEndian.Uint32(sc.swapWords(input[16:20]))) / 100
+	sc.Status.L2Voltage = float32(
+		binary.BigEndian.Uint32(sc.swapWords(input[20:24]))) / 100
+	sc.Status.L3Voltage = float32(
+		binary.BigEndian.Uint32(sc.swapWords(input[24:28]))) / 100
+	sc.Status.L1Current = float32(
+		binary.BigEndian.Uint32(sc.swapWords(input[28:32]))) / 1000
+	sc.Status.L2Current = float32(
+		binary.BigEndian.Uint32(sc.swapWords(input[32:36]))) / 1000
+	sc.Status.L3Current = float32(
+		binary.BigEndian.Uint32(sc.swapWords(input[36:40]))) / 1000
+	sc.Status.ActivePower = float32(
+		binary.BigEndian.Uint32(sc.swapWords(input[40:44]))) * 10
+	sc.Status.ReactivePower = float32(
+		binary.BigEndian.Uint32(sc.swapWords(input[44:48])))
+	sc.Status.ApparentPower = float32(
+		binary.BigEndian.Uint32(sc.swapWords(input[48:52])))
+	sc.Status.PowerFactor = float32(
+		binary.BigEndian.Uint32(sc.swapWords(input[52:56]))) / 1000
+	sc.Status.Energy =
+		float32(binary.BigEndian.Uint32(sc.swapWords(input[56:60]))) / 100
+	sc.Status.MaxPower = float32(
+		binary.BigEndian.Uint32(sc.swapWords(input[60:64])))
+	sc.Status.CurrentChargePower = float32(
+		binary.BigEndian.Uint32(sc.swapWords(input[64:68])))
+	sc.Status.Frequency = float32(
+		binary.BigEndian.Uint32(sc.swapWords(input[68:72])))
+	sc.Status.L1MaxPower = float32(
+		binary.BigEndian.Uint32(sc.swapWords(input[72:76])))
+	sc.Status.L2MaxPower = float32(
+		binary.BigEndian.Uint32(sc.swapWords(input[76:80])))
+	sc.Status.L3MaxPower = float32(
+		binary.BigEndian.Uint32(sc.swapWords(input[80:84])))
 	sc.Status.OverCurrentProtection = binary.BigEndian.Uint16(input[84:86])
 
 	return nil
+}
+
+func (sc StatusCache) swapWords(data []byte) []byte {
+	// TODO: Proper error handling needed.
+	if len(data) != 4 {
+		return data
+	}
+
+	data_r := make([]byte, 0)
+	data_r = append(data_r, data[2:4]...)
+	data_r = append(data_r, data[0:2]...)
+
+	return data_r
 }
 
 func (sc StatusCache) WriteFormattedStatus(out io.Writer) {
@@ -289,19 +319,19 @@ func (sc StatusCache) WriteFormattedStatus(out io.Writer) {
 	fmt.Fprintf(out, "DIP configuration: %d\n", sc.Status.DIPConfiguration)
 	fmt.Fprintf(out, "Firmware Version: %d\n", sc.Status.FirmwareVersion)
 	fmt.Fprintf(out, "Error state: %+v\n", sc.Status.Errorcode)
-	fmt.Fprintf(out, "Voltage [V]: L1 %d, L2 %d, L3 %d\n", sc.Status.L1Voltage,
+	fmt.Fprintf(out, "Voltage [V]: L1 %.2f L2 %.2f, L3 %.2f\n", sc.Status.L1Voltage,
 		sc.Status.L2Voltage, sc.Status.L3Voltage)
-	fmt.Fprintf(out, "Current [A]: L1 %d, L2 %d, L3 %d\n", sc.Status.L1Current,
+	fmt.Fprintf(out, "Current [A]: L1 %.2f, L2 %.2f, L3 %.2f\n", sc.Status.L1Current,
 		sc.Status.L2Current, sc.Status.L3Current)
-	fmt.Fprintf(out, "Active power [W]: %d\n", sc.Status.ActivePower)
-	fmt.Fprintf(out, "Reactive power [W]: %d\n", sc.Status.ReactivePower)
-	fmt.Fprintf(out, "Apparent power [VA]: %d\n", sc.Status.ApparentPower)
-	fmt.Fprintf(out, "Power factor: %d\n", sc.Status.PowerFactor)
-	fmt.Fprintf(out, "Energy [kWh]: %d\n", sc.Status.Energy)
-	fmt.Fprintf(out, "Max Power [W]: %d\n", sc.Status.MaxPower)
-	fmt.Fprintf(out, "Current Charge Power [W]: %d\n", sc.Status.CurrentChargePower)
-	fmt.Fprintf(out, "Frequency [Hz]: %d\n", sc.Status.Frequency)
-	fmt.Fprintf(out, "Max Power [W]: L1 %d, L2 %d, L3 %d\n", sc.Status.L1MaxPower,
+	fmt.Fprintf(out, "Active power [W]: %.2f\n", sc.Status.ActivePower)
+	fmt.Fprintf(out, "Reactive power [W]: %.2f\n", sc.Status.ReactivePower)
+	fmt.Fprintf(out, "Apparent power [VA]: %.2f\n", sc.Status.ApparentPower)
+	fmt.Fprintf(out, "Power factor: %.2f\n", sc.Status.PowerFactor)
+	fmt.Fprintf(out, "Energy [kWh]: %.2f\n", sc.Status.Energy)
+	fmt.Fprintf(out, "Max Power [W]: %.2f\n", sc.Status.MaxPower)
+	fmt.Fprintf(out, "Current Charge Power [W]: %.2f\n", sc.Status.CurrentChargePower)
+	fmt.Fprintf(out, "Frequency [Hz]: %.2f\n", sc.Status.Frequency)
+	fmt.Fprintf(out, "Max Power [W]: L1 %.2f, L2 %.2f, L3 %.2f\n", sc.Status.L1MaxPower,
 		sc.Status.L2MaxPower, sc.Status.L3MaxPower)
 	fmt.Fprintf(out, "Overcurrent protection: %d\n", sc.Status.OverCurrentProtection)
 	fmt.Fprintf(out, "Digital inputs: %+v\n",
